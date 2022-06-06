b88a469195b47a394039f8e1a762bfe5b7ee27c6
Alexinah <alexinamoraa2017@gmail.com>
2022-06-06 12:47:41 +0000

    More returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+    """Returns a tuple with length of string and first char."""
+    length = len(sentence)
+    if length = 0:
+        return None
+    print("Length: {:d} - First character: {}".format(length, sentence[0]))
