487babaf6bdba411d7fcb4eff37f96f6f6c64aec
Alexinah <alexinamoraa2017@gmail.com>
2022-06-06 11:09:38 +0000

    Retrieve an elemnt

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    
+    """Retrieve an elemnt from a list."""
     if idx < 0 or idx > (len(my_list) - 1):
         return None
-    else:
-        elm = my_list.pop([idx])
-        print("Element at index {:d} is {}".format(idx, elm))
-
+    return (my_list[idx])
