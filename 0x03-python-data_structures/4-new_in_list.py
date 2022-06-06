1f562f67d5d8ab06d88835844d4dd613f39de035
Alexinah <alexinamoraa2017@gmail.com>
2022-06-06 11:56:07 +0000

    New

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
+
 def new_in_list(my_list, idx, element):
-    """Replace an element in a list at a specific pos without modifying original list."""
+    """Replace an element in a list at a specific pos without mod list."""
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
 
