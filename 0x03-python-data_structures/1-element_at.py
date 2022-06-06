1cb0137b67af88fa9aad32c0c583243221987e3b
Alexinah <alexinamoraa2017@gmail.com>
2022-06-06 11:06:38 +0000

    elm

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,5 +4,7 @@ def element_at(my_list, idx):
     
     if idx < 0 or idx > (len(my_list) - 1):
         return None
-    return (my_list[idx])
+    else:
+        elm = my_list.pop([idx])
+        print("Element at index {:d} is {}".format(idx, elm))
 
