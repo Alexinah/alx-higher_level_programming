e94329d25c8cb902502b681f41edbd4abf0868ec
Alexinah <alexinamoraa2017@gmail.com>
2022-06-06 13:28:19 +0000

    mul 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@
 
 def divisible_by_2(my_list=[]):
     """Finds all multiples of 2"""
-    for i in range len(my_list):
+    for i in range(len(my_list):
         if my_list[i] % 2 == 0:
             print("{:d} {:s} divisible by 2".format(my_list[i], "is"
                     if my_list[i] else "is not"))
