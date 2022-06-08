b9d25af0c3cc0b57e2ff32e0cde0b80326e28039
Alexinah <alexinamoraa2017@gmail.com>
2022-06-08 14:08:06 +0000

    Weight average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,13 +2,12 @@
 
 
 def weight_average(my_list=[]):
-    if (len(my_list) == 0):
+    if not isinstance(my_list, list) or len(my_list) == 0:
         return 0
 
-    sum = 0
-    for i in my_list[i]:
-        sum += i
-
-        average = sum / (len(my_list))
-
-        print("Average: {:0.2f}".format(average))
+    add = 0
+    size = 0
+    for tup in my_list:
+        add += (tup[0] * tup[1])
+        size += tup[1]
+        return (add / size)
