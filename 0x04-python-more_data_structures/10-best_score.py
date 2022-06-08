321b033172b04830e9c862bf2181bf76d21a8592
Alexinah <alexinamoraa2017@gmail.com>
2022-06-08 12:12:42 +0000

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,12 @@
 
 
 def best_score(a_dictionary):
-    for i in a_dictionary:
-        if i  > a_dictionary[i]:
-            return i
-        else:
-            return None
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+    retu = list(a_dictionary.keys())[0]
+    best = a_dictionary[retu]
+    for i, j in a_dictionary.items():
+        if j > best:
+            best = j
+            retu = i
+    return (retu)
