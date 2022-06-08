ac1f68ba588431ebf55c063def81d48babe34aac
Alexinah <alexinamoraa2017@gmail.com>
2022-06-08 13:34:15 +0000

    Trial

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+import math
+def roman_to_int(roman_string):
+    Ourange = int(1, 4000)
+
+    if (len(roman_string = "") or roman_string is not string):
+        return None
+    else:
+        romann_string = int(roman_string, Ourange)
+        print("{} = {}".format(roman_string, romann_string))
+
+
+
