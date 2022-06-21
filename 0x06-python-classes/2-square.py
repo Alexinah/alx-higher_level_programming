df3e59007d328881eda59416a03b9702281e27ce
Alexinah <alexinamoraa2017@gmail.com>
2022-06-21 15:31:05 +0000

    Class Square.

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,7 +12,3 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >=0")
         self.__size = size
-
-        def getArea(self):
-            """Returns the current area of the square."""
-            return (self.__size * self.__size)
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Class that pefines a square."""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size=0):
+        """Initializes an instance """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        def area(self):
+            return (self.__size * self.__size)
