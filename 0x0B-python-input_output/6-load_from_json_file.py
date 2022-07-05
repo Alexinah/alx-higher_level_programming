73cc458ee3c87a9f1e199bd764ade2c54dde7662
Alexinah <alexinamoraa2017@gmail.com>
2022-07-05 19:13:33 +0000

    Python object

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Function that creates an Object from JSON file"""
+import json
 
 
 def load_from_json_file(filename):
     """Create an object from JSON"""
-    with open(filename) as p:
-        return json.load(p)
+    with open(filename) as f:
+        return json.load(f)
